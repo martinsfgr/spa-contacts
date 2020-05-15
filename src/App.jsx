90a80338e705c92a1e1b27cd3e59f43f5f447dc9
Mauricio Fagner <52453558+martinsfgr@users.noreply.github.com>
2020-05-15 18:36:49 -0300

    Criar componentes da página

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Topbar from "./components/Topbar";
+import Filters from "./components/Filters";
+import Contacts from "./components/Contacts"
+
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Topbar />
+        <Filters />
+        <Contacts />
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <article className="contact" key={this.props.data.id} data-testid="contact">
+        <span className="contact__avatar"><img src={this.props.data.avatar} alt="" /></span>
+        <span className="contact__data">{this.props.data.name}</span>
+        <span className="contact__data">{this.props.data.phone}</span>
+        <span className="contact__data">{this.props.data.country}</span>
+        <span className="contact__data">{this.props.data.admissionDate}</span>
+        <span className="contact__data">{this.props.data.company}</span>
+        <span className="contact__data">{this.props.data.department}</span>
+      </article>
+    );
+  }
+}
+
+export default Contact;
